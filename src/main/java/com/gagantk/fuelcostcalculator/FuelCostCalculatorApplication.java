d0c67b6e71f6b68ae5c464b99e172e8eaa3e3671
Gagan T K <gagantk07@gmail.com>
2021-03-05 20:40:35 +0530

    fix maven install

--- src/main/java/com/gagantk/fuelcostcalculator/FuelCostCalculatorApplication.java
+++ src/main/java/com/gagantk/fuelcostcalculator/FuelCostCalculatorApplication.java
@@ -1,27 +1,15 @@
 package com.gagantk.fuelcostcalculator;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 
-import com.gagantk.fuelcostcalculator.service.FuelCostService;
-
 @SpringBootApplication
 @EnableCaching
-public class FuelCostCalculatorApplication implements CommandLineRunner {
-
-    @Autowired
-    private FuelCostService fuelCostService;
+public class FuelCostCalculatorApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FuelCostCalculatorApplication.class, args);
-	}
-	
-	@Override
-	public void run(String... args) throws Exception {
-        fuelCostService.getFuelEvent();
 	}
 
 }
---
+++ src/main/java/com/gagantk/fuelcostcalculator/component/MessageQueueLoader.java
@@ -0,0 +1,25 @@
+package com.gagantk.mqtt.fuelevent.component;
+
+import java.util.concurrent.TimeUnit;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+
+import com.gagantk.fuelcostcalculator.service.FuelCostService;
+
+@Component
+@Profile("!test")
+public class MessageQueueLoader implements CommandLineRunner {
+
+    @Autowired
+    private FuelCostService fuelCostService;
+    
+    @Override
+	public void run(String... args) throws Exception {
+        fuelCostService.getFuelEvent();
+	}
+
+}
+ 
